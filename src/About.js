26577ad36234c798d0fc16e1afa093f1b6d1d62a
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-16 13:46:04 -0700

    added final small changes

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ export default class About extends Component {
   render() {
     return (
       <div className="container">
-        <h1>About my app</h1>
+        <h1>About our app</h1>
+        <p>This app is designed to pull data from a sports API and render team info and statistics. </p>
       </div>
     );
   }
